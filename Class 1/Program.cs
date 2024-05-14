40215394cbf5d3b57799bae1cd49d8b93b0d46f6
ismayilial <ismayilial@JUPITER16>
2024-05-14 17:19:52 +0400

    Add project files.

---
+++ Class 1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Class_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //variables
+            string firstPersonName = "Ismayil";
+            int firstPersonAge = 25;
+            string firstPersonBirtdate = "13.06.1999";
+
+            string secondPersonName = "Ebru";
+            int secondPersonAge = 23;
+            string secondPersonBirtdate = "13.05.2001";
+
+            
+            
+            Console.WriteLine(firstPersonBirtdate);
+
+
+
+
+
+
+
+            
+        }
+    }
+}
