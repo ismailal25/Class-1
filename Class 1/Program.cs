a0ba143c3480d3db7e1059b927eb68e2d3dbe5bb
Aliyev Ismayil <ismayilial@code.local>
2024-05-14 17:24:36 +0400

    hi

--- Class 1/Program.cs
+++ Class 1/Program.cs
@@ -17,6 +17,8 @@
             
             
             Console.WriteLine(firstPersonBirtdate);
+            Console.WriteLine(firstPersonBirtdate);
+            Console.WriteLine(firstPersonBirtdate);
 
 
 
